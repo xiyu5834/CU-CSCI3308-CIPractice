63a5a6582d81d77ea404a3d54101cf57822d4d40
Yuan Xinyang <xiyu5834@colorado.edu>
2018-04-19 18:32:05 -0600

    change_3

--- geometry.c
+++ geometry.c
@@ -71,5 +71,5 @@ void coord_2d_midpoint(coord_2d_t* mid, const coord_2d_t* a, const coord_2d_t* b
 }
 double coord_2d_area_triangle(const coord_2d_t* a, const coord_2d_t* b, const coord_2d_t* c)
 {
-	return fabs ((a->x*(b->y-c->y)+ b->x*(c->y-a->y) + c->x*(a->y-b->y)/2);
+	return fabs((a->x*(b->y-c->y)+ b->x*(c->y-a->y) + c->x*(a->y-b->y))/2);
 }
